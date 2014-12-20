890c64dff3bfa53d074d4112a90002c305e8740a
Cameron Little <cameron@camlittle.com>
2014-12-19 18:35:15 -0800

    Fixed up project structure and build.

---
+++ SwiftPhotos/SwiftPhotos-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef SwiftPhotos-Bridging-Header
+#define SwiftPhotos-Bridging-Header
+
+#import	"FileSystemEvents/Project/Sources/EonilFileSystemEvents.h"
+#endif
